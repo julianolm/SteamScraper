5e2212a04bff9c925177012ff5a1a178d0461121
Juliano Amadeu <65794514+julianolm@users.noreply.github.com>
2023-09-27 09:38:02 -0300

    More config

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { suma } from "src/paths";
+
+console.log("Hello World!");
+console.log(suma(1, 2));
---
+++ src/paths.ts
@@ -0,0 +1,8 @@
+export function suma(a: number, b: number) {
+  return a + b;
+}
+// Path
+// div com id="tabletGrid" contem o conteudo da pagina
+//
+// Titulo
+// #tabletGrid > div.page_content_ctn > div > div.page_title_area.game_title_area > h2
